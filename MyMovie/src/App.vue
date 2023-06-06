da67fe9c52dad2dc741596132fa6f0155572f9da
ahmadreza tahmasbi <ahmadreza.tmb@gmail.com>
2023-06-06 11:31:15 +0330

    [feat] impelement Trailer component

--- MyMovie/src/App.vue
+++ MyMovie/src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import SuggestionBox from './components/SuggestionBox.vue';
 import SideBarMoiveCard from './components/SideBarMoiveCard.vue';
+import Trailer from './components/Trailer.vue'
 </script>
 
 <template>
   <!-- <SuggestionBox background-image="../src/assets/plus.png" title="Amelie" description="Naive girl in paris" ></SuggestionBox> -->
-  <SideBarMoiveCard coverImage="../src/assets/TLOU.jpg" imdbRate="8.3" title="The Last Of Us" :genres="['action', 'horror']" ></SideBarMoiveCard>
+  <!-- <SideBarMoiveCard coverImage="../src/assets/TLOU.jpg" imdbRate="8.3" title="The Last Of Us" :genres="['action', 'horror']" ></SideBarMoiveCard> -->
+  <Trailer title="Hello" imageBackground="../src/assets/TLOU.jpg"></Trailer>
 </template>
 
 <style scoped>
---
+++ MyMovie/src/components/Trailer.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="trailer-container">
+        <!-- image background doesn't work ??????????????? -->
+        <div
+        :class="'bg-[url('+ imageBackground +')]'"
+        class="h-40 w-[23%] min-w-[14rem] bg-center bg-cover rounded-2xl overflow-hidden"
+        >
+        <div
+            class="overlay bg-slate-800 bg-opacity-60 w-full h-full p-4 text-white flex flex-col justify-center"
+        >
+            <div class="flex justify-center">
+            <div
+                class="bg-slate-200 bg-opacity-20 inline-block p-3 rounded-full"
+            >
+                <img
+                src="../assets/play-btn.png"
+                alt="play btn"
+                class="w-6 h-6"
+                />
+            </div>
+            </div>
+        </div>
+        </div>
+        <div class="pt-2 text-white">{{title}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    props : {
+        title : String,
+        imageBackground : String
+    }
+};
+</script>
