23cdef206a6dff9cbfad40ec2a4915fcb1e70746
ahmadreza tahmasbi <ahmadreza.tmb@gmail.com>
2023-07-07 23:41:38 +0330

    [feat] implement main left side bar

--- MyMovie/src/App.vue
+++ MyMovie/src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <MainMovieCardVue :genreID="obj.genre_ids[0]" :title="obj.title" :src="obj.poster_path" :rating="obj.vote_average" :releaseDate="obj.release_date"></MainMovieCardVue>
+  <div class="bg-[#19191E]">
+    <MainLeftSideBar active-page="Logout"></MainLeftSideBar>
+  </div>
   <!-- <SideBarMoiveCard title="sag" coverImage="../src/assets/TLOU.jpg" :genres="['hello', 'pass']"></SideBarMoiveCard> -->
 </template>
 
@@ -11,30 +13,16 @@ import RemoveFromWatchlistBtn from './components/general/RemoveFromWatchlistBtn.
 import RedLinkBtn from './components/general/RedLinkBtn.vue';
 import ImbdContainer from './components/general/ImbdContainer.vue';
 import MainMovieCardVue from './components/general/MainMovieCard.vue';
-const obj =  
-{
-  "adult": false,
-  "backdrop_path": "/27u4kBGGOQLqizEudJAOWMkvhip.jpg",
-  "genre_ids": [
-    28,
-    80,
-    53
-  ],
-  "id": 385687,
-  "original_language": "en",
-  "original_title": "Fast X",
-  "overview": "Over many missions and against impossible odds, Dom Toretto and his family have outsmarted, out-nerved and outdriven every foe in their path. Now, they confront the most lethal opponent they've ever faced: A terrifying threat emerging from the shadows of the past who's fueled by blood revenge, and who is determined to shatter this family and destroy everything—and everyone—that Dom loves, forever.",
-  "popularity": 3576.187,
-  "poster_path": "/fiVW06jE7z9YnO4trhaMEdclSiC.jpg",
-  "release_date": "2023-05-17",
-  "title": "Fast X",
-  "video": false,
-  "vote_average": 7.3,
-  "vote_count": 2382
-}
-import {client} from './utils';
-const res = client('https://api.themoviedb.org/3/genre/movie/list');
-res.then(data => console.log(data))
+import RowOfMainMovieCard from './components/general/RowOfMainMovieCard.vue'
+import MainRightSideBar from './components/general/MainRightSideBar.vue';
+MainLeftSideBar
+import {client} from './utils'
+import { ref } from 'vue';
+import MainLeftSideBar from './components/general/MainLeftSideBar.vue';
+const objs = ref('');
+const res = client('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1');
+res.then(data => objs.value = data.results)
+
 </script>
 
 <style scoped>
---
+++ MyMovie/src/components/general/MainLeftSideBar.vue
@@ -0,0 +1,116 @@
+<template>
+    <nav class="navbar w-[20%] h-auto pb-4 hidden sm:block">
+      <div class="flex justify-end p-8 sm:hidden">
+        <img
+          id="close-menu-btn"
+          src="../static/plus.png"
+          class="w-7 h-7 rotate-45"
+          alt="close-btn"
+        />
+      </div>
+      <div class="sticky top-0">
+        <!-- logo -->
+        <logo-container />
+        <!-- main links -->
+        <section class="text-sm pb-3 pt-3">
+          <h2 class="nav-section-header">Personal</h2>
+          <navBarItem title="Home" :is-active="activePage == 'Home'"  href="#" />
+          <navBarItem title="Browse" :is-active="activePage == 'Browse'" href="#" />
+          <navBarItem title="Watchlist" :is-active="activePage == 'Watchlist'" href="#" />
+        </section>
+        <section class="text-sm pb-3 pt-3">
+          <h2 class="nav-section-header">Menu</h2>
+          <navBarItem title="Movies" :is-active="activePage == 'Movies'" href="#" />
+          <navBarItem title="TVshows" :is-active="activePage == 'TVshows'" href="#" />
+          <navBarItem title="People" :is-active="activePage == 'People'" href="#" />
+        </section>
+        <section class="text-sm pb-3 pt-3">
+          <h2 class="nav-section-header">general</h2>
+          <navBarItem title="Setting" :is-active="activePage == 'Setting'" href="#" />
+          <navBarItem title="Logout" :is-active="activePage == 'Logout'" href="#" />
+        </section>
+        <!-- notification container -->
+        <div class="notification-container">
+          <br />
+          <br />
+        </div>
+        <!-- tahmasious github -->
+        <div
+          class="github-link-container bg-[#131519] w-[80%] rounded-2xl table m-auto mt-10"
+        >
+          <img
+            src="../static/tahmasious.jpg"
+            alt="owner profile image"
+            width="50px"
+            height="50px"
+            class="rounded-2xl table m-auto relative top-[-20px]"
+          />
+          <h3 class="m-auto text-slate-500 text-center mt-[-15px]">
+            Tahmasious
+          </h3>
+          <h4 class="m-auto text-slate-700 text-center">Owner and dev</h4>
+          <a
+            target="_blank"
+            class="bg-slate-700 table m-auto text-white pl-4 pr-4 pt-2 pb-2 rounded-2xl mb-2 mt-2"
+            href="https://github.com/tahmasious"
+            >Visit github</a
+          >
+        </div>
+      </div>
+    </nav>
+</template>
+
+<script setup>
+import LogoContainer from "./LogoContainer.vue";
+import navBarItem from './NavBarItem.vue';
+defineProps(['activePage'])
+</script>
+
+<style scoped>
+.nav-section-header {
+    @apply pl-9 text-sm mb-5 text-secondary ml-[3px];
+  }
+
+  .navbar {
+  background: rgb(33, 35, 41);
+  background: -moz-linear-gradient(
+    180deg,
+    rgba(33, 35, 41, 1) 0%,
+    rgba(31, 32, 39, 1) 23%,
+    rgba(29, 30, 36, 1) 50%,
+    rgba(26, 28, 32, 1) 75%,
+    rgba(25, 27, 31, 1) 90%,
+    rgba(24, 26, 30, 1) 100%
+  );
+  background: -webkit-linear-gradient(
+    180deg,
+    rgba(33, 35, 41, 1) 0%,
+    rgba(31, 32, 39, 1) 23%,
+    rgba(29, 30, 36, 1) 50%,
+    rgba(26, 28, 32, 1) 75%,
+    rgba(25, 27, 31, 1) 90%,
+    rgba(24, 26, 30, 1) 100%
+  );
+  background: linear-gradient(
+    180deg,
+    rgba(33, 35, 41, 1) 0%,
+    rgba(31, 32, 39, 1) 23%,
+    rgba(29, 30, 36, 1) 50%,
+    rgba(26, 28, 32, 1) 75%,
+    rgba(25, 27, 31, 1) 90%,
+    rgba(24, 26, 30, 1) 100%
+  );
+  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#212329",endColorstr="#181a1e",GradientType=1);
+}
+
+.nav-section-header {
+  margin-bottom: 1.25rem;
+  margin-left: 3px;
+  padding-left: 2.25rem;
+  font-size: 0.875rem;
+  line-height: 1.25rem;
+  --tw-text-opacity: 1;
+  color: rgb(73 75 87 / var(--tw-text-opacity));
+}
+
+</style>
