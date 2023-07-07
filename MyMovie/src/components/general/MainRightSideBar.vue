58d7d8643a5d64ab0e690e86d13daa4cc66fca99
ahmadreza tahmasbi <ahmadreza.tmb@gmail.com>
2023-07-07 23:16:04 +0330

    [feat] add right sidebar component

---
+++ MyMovie/src/components/general/MainRightSideBar.vue
@@ -0,0 +1,84 @@
+<template>
+    <aside class="w-[25%] p-5 hidden sm:block">
+      <section class="flex flex-col gap-8 sticky top-5">
+        <!-- top rated movies bar -->
+        <section>
+          <h2 class="mb-4 text-white">Top rated movies</h2>
+          <div class="top-rated-movies text-sm">
+            <SideBarMoiveCard v-for="movie in topRatedMovies" :key="movie.id" :cover-image-path="movie.poster_path" :title="movie.title" :genres="movie.genre_ids" :imdb-rate="movie.vote_average"/>
+          </div>
+          <RedLinkBtn text="see all" href="#" />
+        </section>
+        <!-- upcoming movies bar -->
+        <section>
+          <h2 class="mb-4 text-white">Upcoming movies</h2>
+          <div class="upcoming-movies text-sm">
+            <SideBarMoiveCard v-for="movie in upcomingMoives" :key="movie.id" :cover-image-path="movie.poster_path" :title="movie.title" :genres="movie.genre_ids" :imdb-rate="movie.vote_average"/>
+          </div>
+          <RedLinkBtn text="see all" href="#" />
+        </section>
+      </section>
+    </aside>
+</template>
+
+
+<script setup>
+import { ref } from 'vue';
+import {client} from './../../utils.js'
+import SideBarMoiveCard from './SideBarMoiveCard.vue';
+import RedLinkBtn from './RedLinkBtn.vue'
+
+
+const topRatedMovies = ref([]);
+const upcomingMoives = ref([]);
+
+const res = client('https://api.themoviedb.org/3/movie/top_rated');
+res.then(data => {
+  for(let i = 0 ; i < 3 ; i++){
+    topRatedMovies.value.push(data.results[i])
+  }
+})
+
+const secRes = client('https://api.themoviedb.org/3/movie/upcoming');
+secRes.then(data => {
+  for(let i = 0 ; i < 3 ; i++){
+    upcomingMoives.value.push(data.results[i])
+  }
+})
+
+</script>
+
+<style scoped>
+aside {
+  background: rgb(33, 35, 41);
+  background: -moz-linear-gradient(
+    180deg,
+    rgba(33, 35, 41, 1) 0%,
+    rgba(31, 32, 39, 1) 23%,
+    rgba(29, 30, 36, 1) 50%,
+    rgba(26, 28, 32, 1) 75%,
+    rgba(25, 27, 31, 1) 90%,
+    rgba(24, 26, 30, 1) 100%
+  );
+  background: -webkit-linear-gradient(
+    180deg,
+    rgba(33, 35, 41, 1) 0%,
+    rgba(31, 32, 39, 1) 23%,
+    rgba(29, 30, 36, 1) 50%,
+    rgba(26, 28, 32, 1) 75%,
+    rgba(25, 27, 31, 1) 90%,
+    rgba(24, 26, 30, 1) 100%
+  );
+  background: linear-gradient(
+    180deg,
+    rgba(33, 35, 41, 1) 0%,
+    rgba(31, 32, 39, 1) 23%,
+    rgba(29, 30, 36, 1) 50%,
+    rgba(26, 28, 32, 1) 75%,
+    rgba(25, 27, 31, 1) 90%,
+    rgba(24, 26, 30, 1) 100%
+  );
+  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#212329",endColorstr="#181a1e",GradientType=1);
+}
+
+</style>
