716979d72642ad48208164585403564712943863
ahmadreza tahmasbi <ahmadreza.tmb@gmail.com>
2023-07-08 15:30:12 +0330

    [feat] add test live search component

--- MyMovie/src/App.vue
+++ MyMovie/src/App.vue
@@ -1,37 +1,31 @@
 <template>
-  <div class="bg-[#19191E]">
-    <RowsOfOptionedMovieCards 
-    :options="
-    {'Top Rated' : 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', 
-    'Up comming' : 'https://api.themoviedb.org/3/movie/upcoming', 
-    'Now playing' : 'https://api.themoviedb.org/3/movie/now_playing'}">
-      Movies
-    </RowsOfOptionedMovieCards>
-  </div>
-  <!-- <SideBarMoiveCard title="sag" coverImage="../src/assets/TLOU.jpg" :genres="['hello', 'pass']"></SideBarMoiveCard> -->
+  <main-lay-out/>
+
+
 </template>
 
 <script setup>
-import LogoContainerVue from './components/general/LogoContainer.vue';
-import NavBarItem from './components/general/NavBarItem.vue';
-import SideBarMoiveCard from './components/general/SideBarMoiveCard.vue';
-import RemoveFromWatchlistBtn from './components/general/RemoveFromWatchlistBtn.vue'
-import RedLinkBtn from './components/general/RedLinkBtn.vue';
-import ImbdContainer from './components/general/ImbdContainer.vue';
-import MainMovieCardVue from './components/general/MainMovieCard.vue';
-import RowOfMainMovieCard from './components/general/RowOfMainMovieCard.vue'
-import MainRightSideBar from './components/general/MainRightSideBar.vue';
+// import LogoContainerVue from './components/general/LogoContainer.vue';
+// import NavBarItem from './components/general/NavBarItem.vue';
+// import SideBarMoiveCard from './components/general/SideBarMoiveCard.vue';
+// import RemoveFromWatchlistBtn from './components/general/RemoveFromWatchlistBtn.vue'
+// import RedLinkBtn from './components/general/RedLinkBtn.vue';
+// import ImbdContainer from './components/general/ImbdContainer.vue';
+// import MainMovieCard from './components/general/MainMovieCard.vue';
+// import RowOfMainMovieCard from './components/general/RowOfMainMovieCard.vue'
+// import MainRightSideBar from './components/general/MainRightSideBar.vue';
 
-import {client} from './utils'
-import { ref } from 'vue';
-import MainLeftSideBar from './components/general/MainLeftSideBar.vue';
+// import {client} from './utils'
+// import { computed, ref, watch } from 'vue';
+// import MainLeftSideBar from './components/general/MainLeftSideBar.vue';
 import MainLayOut from './components/general/MainLayOut.vue';
-import RowsOfOptionedMovieCards from './components/general/RowsOfOptionedMovieCards.vue';
-const objs = ref('');
-const res = client('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1');
-res.then(data => {
-  console.log(data)
-  return objs.value = data.results})
+// import RowsOfOptionedMovieCards from './components/general/RowsOfOptionedMovieCards.vue';
+// const objs = ref('');
+// const res = client('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1');
+// res.then(data => {
+//   console.log(data)
+//   return objs.value = data.results})
+
 
 </script>
 
