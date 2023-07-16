182647b67e88b296cf52dc30c484105c74082a7d
ahmadreza tahmasbi <ahmadreza.tmb@gmail.com>
2023-07-16 18:22:27 +0330

    [fix] remove loggin check codes

--- MyMovie/src/App.vue
+++ MyMovie/src/App.vue
@@ -1,31 +1,11 @@
 <template>
-  <!-- <RouterView /> -->
-  <TestLiveSearch />
+  <RouterView />
 </template>
 
 <script setup>
-import {RouterView, RouterLink} from "vue-router";
-// import LogoContainerVue from './components/general/LogoContainer.vue';
-// import NavBarItem from './components/general/NavBarItem.vue';
-// import SideBarMoiveCard from './components/general/SideBarMoiveCard.vue';
-// import RemoveFromWatchlistBtn from './components/general/RemoveFromWatchlistBtn.vue'
-// import RedLinkBtn from './components/general/RedLinkBtn.vue';
-// import ImbdContainer from './components/general/ImbdContainer.vue';
-// import MainMovieCard from './components/general/MainMovieCard.vue';
-// import RowOfMainMovieCard from './components/general/RowOfMainMovieCard.vue'
-// import MainRightSideBar from './components/general/MainRightSideBar.vue';
-// import TestLiveSearch from './components/homepage/TestLiveSearch.vue'
-// import {client} from './utils'
-// import { computed, ref, watch } from 'vue';
-// import MainLeftSideBar from './components/general/MainLeftSideBar.vue';
-// import MainLayOut from '@/components/general/MainLayOut.vue';
-// import HomePage from '@/components/homepage/HomePage.vue';
-// import RowsOfOptionedMovieCards from './components/general/RowsOfOptionedMovieCards.vue';
-// const objs = ref('');
-// const res = client('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1');
-// res.then(data => {
-//   console.log(data)
-//   return objs.value = data.results})
+import {RouterView} from "vue-router";
 
 
 </script>
+
+
