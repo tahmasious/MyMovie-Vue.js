dd4329aa42b75ef56a1bed0c83b05b26556a5c5e
ahmadreza tahmasbi <ahmadreza.tmb@gmail.com>
2023-07-20 18:09:19 +0330

    [fix] remove extra section in html

--- MyMovie/src/components/general/RowOfMainMovieCard.vue
+++ MyMovie/src/components/general/RowOfMainMovieCard.vue
@@ -1,16 +1,14 @@
 <template>
-    <section>
-        <section class="movie-poster-container mt-8">
-          <div class="flex justify-between mb-3 items-center">
-            <h2 class="text-white text-lg"><slot></slot></h2>
-          </div>
-          <div class="flex justify-between flex-wrap">
-          <div v-for="movie in movies" :key="movie.id">
-            <MainMovieCard :MovieID="movie.id" :genreID="movie.genre_ids[0]" :title="movie.title" :src="movie.poster_path" :rating="movie.vote_average" :releaseDate="movie.release_date"/>
-          </div>
-          </div>
-        </section>
-      </section>
+    <section class="movie-poster-container mt-8">
+      <div class="flex justify-between mb-3 items-center">
+        <h2 class="text-white text-lg"><slot></slot></h2>
+      </div>
+      <div class="flex justify-between flex-wrap">
+      <div v-for="movie in movies" :key="movie.id">
+        <MainMovieCard :MovieID="movie.id" :genreID="movie.genre_ids[0]" :title="movie.title" :src="movie.poster_path" :rating="movie.vote_average" :releaseDate="movie.release_date"/>
+      </div>
+      </div>
+    </section>
 </template>
 
 
