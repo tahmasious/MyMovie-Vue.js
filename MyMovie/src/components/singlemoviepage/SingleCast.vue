e9dad170f6fcc9983f60044d363b13d7af70a514
ahmadreza tahmasbi <ahmadreza.tmb@gmail.com>
2023-07-20 17:51:02 +0330

    [feat] implement single cast component

---
+++ MyMovie/src/components/singlemoviepage/SingleCast.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="inline-flex flex-col justify-start items-center mx-3 w-32 mb-4">
+        <img
+            :src="profileImage"
+            alt="cast profile"
+            class="w-[55px] h-[55px] rounded-full object-cover"
+        />
+        <span class="text-white text-sm">{{ name }}</span>
+    </div>
+</template>
+
+<script setup>
+import { API_IMAGE_BASE_URL} from '@/constants/api-constants'
+import { computed } from 'vue'
+import defaultProfileIMage from '@/assets/profile_placeholder.png'
+const props = defineProps(['profile_path', 'name'])
+
+const profileImage = computed(() => {
+    if (props.profile_path){
+        return `${API_IMAGE_BASE_URL}/w185${props.profile_path}`
+    }
+    return defaultProfileIMage;
+})
+</script>
