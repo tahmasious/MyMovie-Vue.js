63875bb2b781f807269f18e4b55bd58725e5a140
ahmadreza tahmasbi <ahmadreza.tmb@gmail.com>
2023-07-20 17:50:36 +0330

    [feat] implement notable cast section

---
+++ MyMovie/src/components/singlemoviepage/NotableCastSection.vue
@@ -0,0 +1,22 @@
+<template>
+    <section class="mb-6">
+        <h2 class="text-secondary mb-2">NOTABLE CAST</h2>
+        <div class="flex flex-wrap justify-start">
+            <SingleCast v-for="cast in data.cast.slice(0,10)" :key="cast.id" :name="cast.name" :profile_path="cast.profile_path" />
+        </div>
+    </section>
+</template>
+
+<script setup>
+import SingleCast from './SingleCast.vue';
+import {useFetch} from '@/composable/useFetch.js'
+import { useRoute } from 'vue-router';
+import {API_BASE_URL} from '@/constants/api-constants'
+import { watch } from 'vue';
+const {data ,isLoading, error, fetchWrapper} = useFetch({'cast' : []})
+const route = useRoute(); 
+fetchWrapper(`${API_BASE_URL}3/movie/${route.params.id}/credits`)
+watch(data, () => {
+    console.log(data.value.cast);
+})
+</script>
