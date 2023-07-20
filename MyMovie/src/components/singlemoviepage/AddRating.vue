2bb2844ea15d7e436ffc6b3a03a0a869700194c8
ahmadreza tahmasbi <ahmadreza.tmb@gmail.com>
2023-07-21 00:51:11 +0330

    [feat] implement rate component

---
+++ MyMovie/src/components/singlemoviepage/AddRating.vue
@@ -0,0 +1,110 @@
+<template>
+    <div
+        class="rate text-white text-lg glss-bg rounded-2xl py-[10px] px-3 relative z-10"
+    >
+        <img
+        src="@/assets/rate.png"
+        alt="rate btn"
+        class="inline-block w-6 h-6"
+        />
+        <div
+        class="rate-box bg-white-glass p-3 hidden absolute w-[200px] left-[-100px]"
+        >
+        <div v-if="error">
+            Error has happend ...
+        </div>
+        <div v-else-if="data.success">
+            Rate submitted successfully
+        </div>
+        <div v-else class="flex-col items-center">
+            <p class="text-sm">How do you rate this movie ?</p>
+            <div class="star-box flex">
+                
+                <img
+                src="@/assets/rate.png"
+                alt="rate btn"
+                class="inline-block w-6 h-6"
+                @click="submitRate(2)"
+                @mouseover="changeColor(1)"
+                @mouseleave="resetColors"
+                />
+                <img
+                src="@/assets/rate.png"
+                alt="rate btn"
+                class="inline-block w-6 h-6"
+                @click="submitRate(4)"
+                @mouseover="changeColor(2)"
+                @mouseleave="resetColors"
+                />
+                <img
+                src="@/assets/rate.png"
+                alt="rate btn"
+                class="inline-block w-6 h-6"
+                @click="submitRate(6)"
+                @mouseover="changeColor(3)"
+                @mouseleave="resetColors"
+                />
+                <img
+                src="@/assets/rate.png"
+                alt="rate btn"
+                class="inline-block w-6 h-6"
+                @click="submitRate(8)"
+                @mouseover="changeColor(4)"
+                @mouseleave="resetColors"
+                />
+                <img
+                src="@/assets/rate.png"
+                alt="rate btn"
+                class="inline-block w-6 h-6"
+                @click="submitRate(10)"
+                @mouseover="changeColor(5)"
+                @mouseleave="resetColors"
+                />
+            </div>
+        </div>
+        </div>
+    </div>
+</template>
+<script setup>
+import {useFetch} from '@/composable/useFetch.js'
+import {API_BASE_URL} from '@/constants/api-constants'
+import { useRoute } from 'vue-router';
+
+const route = useRoute(); 
+
+const {data , isLoading, error,fetchWrapper} = useFetch([])
+const sessionID = sessionStorage.getItem('session_id')
+
+
+
+function submitRate(val) {
+    fetchWrapper(`${API_BASE_URL}3/movie/${route.params.id}/rating?session_id=${sessionID}`, {'value' : val}, 'POST');
+}
+
+function changeColor(val) {
+    const elements = document.querySelectorAll(`.star-box img:not(:nth-child(n+${val+1}))`) 
+    elements.forEach((item) => item.classList.add("red-start"))
+}
+
+function resetColors() {
+    const elements = document.querySelectorAll(`.star-box img`) 
+    elements.forEach((item) => item.classList.remove("red-start"))
+}
+
+</script>
+<style scoped>
+.bg-white-glass {
+  background: rgba(255, 255, 255, 0.55);
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+  backdrop-filter: blur(7px);
+  -webkit-backdrop-filter: blur(7px);
+  border-radius: 10px;
+  border: 1px solid rgba(255, 255, 255, 0.18);
+}
+.rate:hover div {
+  display: flex;
+}
+.red-start {
+    filter: invert(1);
+}
+</style>
--- MyMovie/src/components/singlemoviepage/SingleMoviePage.vue
+++ MyMovie/src/components/singlemoviepage/SingleMoviePage.vue
@@ -53,43 +53,7 @@
                 <!-- add to watchlist btn -->
                 <AddToWatchListBtn :isGlassy="true"></AddToWatchListBtn>
                 <!-- rate btn -->
-                <div
-                  class="rate text-white text-lg glss-bg rounded-2xl py-[10px] px-3 relative z-10"
-                >
-                  <img
-                    src="@/assets/rate.png"
-                    alt="#"
-                    class="inline-block w-6 h-6"
-                  />
-                  <div
-                    class="rate-box bg-white-glass p-3 hidden absolute w-[200px] left-[-100px] flex-col items-center"
-                  >
-                    <p class="text-sm">How do you rate this movie ?</p>
-                    <div class="star-box flex">
-                      <img
-                        src="@/assets/rate.png"
-                        alt="#"
-                        class="inline-block w-6 h-6"
-                      /><img
-                        src="@/assets/rate.png"
-                        alt="#"
-                        class="inline-block w-6 h-6"
-                      /><img
-                        src="@/assets/rate.png"
-                        alt="#"
-                        class="inline-block w-6 h-6"
-                      /><img
-                        src="@/assets/rate.png"
-                        alt="#"
-                        class="inline-block w-6 h-6"
-                      /><img
-                        src="@/assets/rate.png"
-                        alt="#"
-                        class="inline-block w-6 h-6"
-                      />
-                    </div>
-                  </div>
-                </div>
+                <AddRating />
               </div>
             </div>
             <div class="flex justify-between sm:flex-row flex-col gap-2">
@@ -173,6 +137,7 @@ import GlassySearchBarVue from '@/components/general/GlassySearchBar.vue';
 import AddToWatchListBtn from '@/components/general/AddToWatchListBtn.vue';
 import MediaSectionVue from '@/components/singlemoviepage/MediaSection.vue';
 import NotableCastSection from './NotableCastSection.vue';
+import AddRating from './AddRating.vue';
 
 const route = useRoute(); 
 const {data ,isLoading, error, fetchWrapper} = useFetch()
@@ -300,19 +265,6 @@ function goBack() {
 }
 
 
-
-.bg-white-glass {
-  background: rgba(255, 255, 255, 0.55);
-  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
-  backdrop-filter: blur(7px);
-  -webkit-backdrop-filter: blur(7px);
-  border-radius: 10px;
-  border: 1px solid rgba(255, 255, 255, 0.18);
-}
-
-.rate:hover div {
-  display: flex;
-}
 
 .info-item {
     @apply rounded-2xl py-7 w-full;
