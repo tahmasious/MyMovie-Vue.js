cb86de7201e2dd1c0b2639bcd35d309862abc9ee
ahmadreza tahmasbi <ahmadreza.tmb@gmail.com>
2023-07-25 16:17:24 +0330

    [fix] remove extra imports

--- MyMovie/src/components/singlepeoplepage/SinglePeoplePage.vue
+++ MyMovie/src/components/singlepeoplepage/SinglePeoplePage.vue
@@ -6,7 +6,7 @@
                     <img
                         :src="`${API_IMAGE_BASE_URL}/w342${data.profile_path}`"
                         alt="celeb profile"
-                        class="rounded-3xl w-ful sm:w-[25%]"
+                        class="rounded-3xl w-ful sm:w-[25%] sticky top-3"
                     />
                     <div class="bio">
                         <h1 class="mb-4 text-white text-[30px]">{{ data.name }}</h1>
@@ -32,7 +32,7 @@ import MainLayOut from '@/components/general/MainLayOut.vue';
 import RightSideBar from '@/components/singlepeoplepage/RightSideBar.vue'
 import {useFetch} from '@/composable/useFetch.js'
 import { API_IMAGE_BASE_URL} from '@/constants/api-constants'
-import { computed, ref, watch } from 'vue';
+import { watch } from 'vue';
 import SocialSection from '@/components/singlepeoplepage/SocialSection.vue'
 import RowsOfKnownForMovies from '../../components/singlepeoplepage/RowsOfKnownForMovies.vue';
 const route = useRoute(); 
