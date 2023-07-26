f9b50a0c853475d14fa5bcfcb3f328e5eb9301a7
ahmadreza tahmasbi <ahmadreza.tmb@gmail.com>
2023-07-26 15:25:29 +0330

    [feat] put access token into project

--- MyMovie/src/constants/api-constants.js
+++ MyMovie/src/constants/api-constants.js
@@ -2,4 +2,4 @@ export const API_BASE_URL = 'https://api.themoviedb.org/'
 export const API_IMAGE_BASE_URL = 'https://image.tmdb.org/t/p'
 export const API_IMAGE_SIZE = 'w154'
 export const API_VERSION = 3
-export const API_READ_ACCESS_TOKEN = import.meta.env.VITE_API_READ_ACCESS_TOKEN
+export const API_READ_ACCESS_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZTFlZGU1ZWZmZDI3MGY5YzA2ZGZjYmNiYzA5NTFjZSIsInN1YiI6IjY0NjY1MDZlYzM1MTRjMDExZGM5YzAyNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SGUUPrpYPuP0z5LRA3_1xJOG4rxo1yOKa4d8x-6nu68'
